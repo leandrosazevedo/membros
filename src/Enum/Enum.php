a498cc92ca1a3a460c3fe22773c19fb6f8e3fc3b
leandroazevedo <leandroazevedo@brq.com>
2023-07-19 18:28:44 -0300

    ajustes

--- src/Enum/Enum.php
+++ src/Enum/Enum.php
@@ -5,37 +5,31 @@ namespace App\Enum;
 use InvalidArgumentException;
 use ReflectionClass;
 
-abstract class Enum
-{
+abstract class Enum {
+    
     protected $value;
 
-    protected function __construct($value)
-    {
+    protected function __construct($value){
         $this->value = $value;
     }
 
-    public function getValue()
-    {
+    public function getValue(){
         return $this->value;
     }
 
-    public static function isValid($value)
-    {
+    public static function isValid($value){
         return in_array($value, static::getConstants(), true);
     }
 
-    public static function getConstants()
-    {
+    public static function getConstants(){
         $reflection = new ReflectionClass(static::class);
         return $reflection->getConstants();
     }
 
-    public static function fromValue($value)
-    {
+    public static function fromValue($value){
         if (!static::isValid($value)) {
-            throw new InvalidArgumentException("Invalid value for enum " . static::class);
+            throw new InvalidArgumentException("Valor inválido para o enum " . static::class);
         }
-
         return new static($value);
     }
 }
